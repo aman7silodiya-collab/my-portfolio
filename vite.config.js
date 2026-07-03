04080c93d751ff254240bb13f986744ce6bf2c2a
Aman silodiya <aman7silodiya@gmail.com>
2026-07-03 14:29:35 +0530

    fix tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),  // ✅ Ye automatically Tailwind v4 enable karega
-  ],
-});
+  plugins: [react()],
+})
